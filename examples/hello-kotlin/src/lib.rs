5b71db25fc65f2a908946235351beaacce6c678f
Lemuel DLS <noreply@git.lemueldls.dev>
2026-07-30 09:42:30 -0400

    feat: snapshot tests

--- examples/hello-kotlin/src/lib.rs
+++ examples/hello-kotlin/src/lib.rs
@@ -1,20 +1,11 @@
 use facet::Facet;
 
 #[derive(Facet)]
-pub struct UserProfile {
-    pub id: u32,
-    pub active: bool,
+pub struct Payload {
+    pub data: u16,
 }
 
 #[koffi::export]
-pub fn process_user(user: UserProfile, factor: u32) -> bool {
-    user.active && factor > 0
+pub fn hello() -> Payload {
+    Payload { data: 42 }
 }
-
-// #[koffi::export]
-// pub fn resolve(map: rustc_hash::FxHashMap<u8, String>) -> String {
-//     map.into_iter()
-//         .map(|(k, v)| format!("{}: {}", k, v))
-//         .collect::<Vec<_>>()
-//         .join(", ")
-// }
---
+++ tests/examples.rs
@@ -0,0 +1,22 @@
+use std::{fmt::Write, fs, path::PathBuf};
+
+use koffi_build::build_crate;
+use koffi_codegen::extract::extract_schema;
+
+#[test]
+fn examples() -> anyhow::Result<()> {
+    let examples_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../examples");
+
+    let dir = fs::read_dir(&examples_dir).expect("failed to read examples directory");
+    for entry in dir {
+        let entry = entry.expect("failed to read example directory entry");
+        let path = entry.path();
+
+        let (crate_name, cdylib_path) = build_crate(&path, false, &[])?;
+        let schema = extract_schema(crate_name.clone(), &cdylib_path)?;
+
+        insta::assert_debug_snapshot!(crate_name, schema);
+    }
+
+    Ok(())
+}
